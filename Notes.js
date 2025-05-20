4e6cab1461654afc89e40b8c4d28f915971cc20a
Sneha Jadon <93697009+SnehaJadon@users.noreply.github.com>
2025-05-20 09:43:29 +0530

    server setup done

---
+++ Notes.js
@@ -0,0 +1,10 @@
+//! step1
+//* setup server 
+//create server folder 
+//install package.json $npm init -y (if not doing -y you can configure it on terminal)
+//install more packages which we'll use later
+//? npm i bcryptjs cookie-parser cors dotenv i cloudinary express jsonwebtoken mongoose multer nodemon 
+//create server.js as main file and also set it as entry point
+//create .gitignore file and add files that you don't want to push to github
+
+//* create database
